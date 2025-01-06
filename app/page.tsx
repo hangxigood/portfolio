a387302fc5010d88ec3d75575175c3cb765ec74d
hangxigood <hangxigood@gmail.com>
2025-01-05 22:43:24 -0700

    Remove metadata from the main page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-import { Metadata } from "next";
 import { ProfileImage } from "@/components/Profile/ProfileImage";
 import { SocialLinks } from "@/components/Profile/SocialLinks";
 import { Bio } from "@/components/Profile/Bio";
-
-export const metadata: Metadata = {
-  title: "Hangxi Xiang - Software Developer",
-  description:
-    "Personal portfolio of Hangxi Xiang, a software developer based in Calgary with experience in embedded systems, marketing, product management, and data analysis.",
-};
 
 const bioParagraphs = [
   "I used to work as an embedded systems developer, marketer, product manager, and data analyst. These roles have given me great insights into both business and technology.",
